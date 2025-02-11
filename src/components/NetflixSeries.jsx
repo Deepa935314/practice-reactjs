4116665eecbae47ad703c17d4c525506e627a8f9
Deepa935314 <dj441139@gmail.com>
2025-02-11 12:58:10 +0530

    added destructuring on SeriesCard.

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -5,7 +5,7 @@ export const NetflixSeries = () => {
     return (
         <ul>
             {SeriesData.map((curElem) => (
-               <SeriesCard key={curElem.id} curElem={curElem}/>
+               <SeriesCard key={curElem.id} data={curElem}/>
             //    passing data throw curElem  props
             //  iff we are using single line of code no need to use return key word.
             ))}
